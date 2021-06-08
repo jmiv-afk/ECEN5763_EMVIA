e71f2c09f0ec40720d03524832965805176c5320
Jake Michael <james.michael@colorado.edu>
2021-06-07 20:47:20 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+### This repository contains the collective work completed by Jake Michael in ECEN 5763, Embedded Machine Vision and Intelligent Automation (EMVIA) at CU Boulder, summer 2021. 
+
+
---
+++ opencv_detect/opencv_detect.cpp
@@ -0,0 +1,13 @@
+#include "opencv2/opencv.hpp"
+
+using namespace cv;
+using namespace std;
+
+int main ()
+{ 
+  cout << "OpenCV version: " << CV_VERSION << endl;
+  cout << "Major version: " << CV_MAJOR_VERSION << endl;
+  cout << "Minor version: " << CV_MINOR_VERSION << endl;
+  cout << "Subminor version: " << CV_SUBMINOR_VERSION << endl;
+
+}
