e6d7cf043215458a6244d2ff04d5b2474b3ac8d5
Jake Michael <james.michael@colorado.edu>
2021-07-28 16:08:52 -0600

    command line parser in main

--- project/main.cpp
+++ project/main.cpp
@@ -1,10 +1,13 @@
 /* ----------------------------------------------------------------------------
  * @file main.cpp
- * @brief
+ * @brief A prototype self-driving assistant feature for lane detection and 
+ *        departure warnings
  *
  * @author Jake Michael, jami1063@colorado.edu
  * @course ECEN 5763: EMVIA, Summer 2021
- * @resources
+ * @resources 
+ *  - Learning OpenCV3, Kaehler, Bradski
+ *  - Sam Siewerts example codes
  *---------------------------------------------------------------------------*/
 
 #include <stdlib.h>
@@ -14,34 +17,68 @@
 #include <opencv2/videoio.hpp>
 #include <opencv2/highgui.hpp>
 #include <opencv2/video.hpp>
+#include <opencv2/core/utility.hpp>
 #include <opencv2/cudaarithm.hpp>
 #include <opencv2/cudaimgproc.hpp>
 #include "log.h"
 #include "lane.h"
 
-//#define INPUT_VIDEO ("input_video/02260001.AVI")
-#define INPUT_VIDEO ("input_video/22400001.AVI")
-
 using namespace cv;
 using namespace std;
 
+// the keys for the command line arguments
+const String parser_keys =
+  "{help h usage ? | | Print help message. }"
+  "{input i  | input_video/clip1.mp4      | Full filepath to input video.  }"
+  "{output o | output_video/clip1_out.mp4 | Full filepath to output video. }"
+  "{show     | 0 | Shows intermediate image pipeline steps. }"
+  ;
 
+// global variables extracted from the parser - application settings
+String input_video_g;
+String output_video_g;
+bool show_pipeline_g;
+
+//
 // the main program
+//
 int main(int argc, char **argv) {
 
+  // 
+  // command line parsing for application settings  
+  //
+  CommandLineParser parser(argc, argv, parser_keys);
+
+  if (parser.has("help")) {
+    parser.printMessage();
+    return 0;
+  }
+
+  input_video_g = parser.get<String>("input");
+  output_video_g = parser.get<String>("output");
+  show_pipeline_g = parser.get<bool>("show");
+  
+  cout << input_video_g << endl;
+  cout << output_video_g << endl;
+  cout << show_pipeline_g << endl;
+  
+  //
+  // algorithm begin  
+  //
   double prev, start;
   unsigned int framecnt = 0;
   Mat temp, raw, gray, gray_detect, pyr, blurred, thresh, hls;
   Rect roibox;
 
-  //cvNamedWindow("test", CV_WINDOW_AUTOSIZE);
-  cvNamedWindow("ROI-Binary", CV_WINDOW_AUTOSIZE);
-  cvNamedWindow("raw", CV_WINDOW_AUTOSIZE);
-  //cvNamedWindow("gray", CV_WINDOW_AUTOSIZE);
-  cvNamedWindow("gray_detect", CV_WINDOW_AUTOSIZE);
+  if (show_pipeline_g) {
+    cvNamedWindow("test", CV_WINDOW_AUTOSIZE);
+    cvNamedWindow("ROI-Binary", CV_WINDOW_AUTOSIZE);
+    cvNamedWindow("raw", CV_WINDOW_AUTOSIZE);
+    cvNamedWindow("gray", CV_WINDOW_AUTOSIZE);
+    cvNamedWindow("gray_detect", CV_WINDOW_AUTOSIZE);
+  }
 
-  VideoCapture cap(INPUT_VIDEO);
-  cap.set(CAP_PROP_POS_MSEC, 430000);
+  VideoCapture cap(input_video_g);
 
   VideoWriter video_out(
     "output.mp4",
